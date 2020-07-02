4e745195c17e3a49f34317a821f440b76b28de3e
Joshua Sullivan <joshuasullivan@gmail.com>
2020-07-02 16:04:26 -0500

    Initial Commit

---
+++ font-viewer/font_viewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  font_viewerApp.swift
+//  font-viewer
+//
+//  Created by Joshua Sullivan on 7/2/20.
+//
+
+import SwiftUI
+
+@main
+struct font_viewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
